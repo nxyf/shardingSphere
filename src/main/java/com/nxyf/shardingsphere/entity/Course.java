7ec47f48d07781ce7ebf36a52d7d41837a360a50
nxyf <yunjie_nx@163.com>
2021-04-30 11:36:00 +0800

    新增与查询

--- src/main/java/com/nxyf/shardingsphere/entity/Course.java
+++ src/main/java/com/nxyf/shardingsphere/entity/Course.java
@@ -1,5 +1,8 @@
 package com.nxyf.shardingsphere.entity;
 
+import lombok.Data;
+import lombok.ToString;
+
 /**
  * @ClassName Course
  * @Description TODO
@@ -7,7 +10,8 @@ package com.nxyf.shardingsphere.entity;
  * @Date 2021/4/28 15:00
  * @Version 1.0
  **/
-
+@Data
+@ToString
 public class Course {
 
     private Long cid;
@@ -17,36 +21,4 @@ public class Course {
     private Long userId;
 
     private String cstatus;
-
-    public Long getCid() {
-        return cid;
-    }
-
-    public void setCid(Long cid) {
-        this.cid = cid;
-    }
-
-    public String getCname() {
-        return cname;
-    }
-
-    public void setCname(String cname) {
-        this.cname = cname;
-    }
-
-    public Long getUserId() {
-        return userId;
-    }
-
-    public void setUserId(Long userId) {
-        this.userId = userId;
-    }
-
-    public String getCstatus() {
-        return cstatus;
-    }
-
-    public void setCstatus(String cstatus) {
-        this.cstatus = cstatus;
-    }
 }
--- src/test/java/com/nxyf/shardingsphere/ShardingSphereApplicationTests.java
+++ src/test/java/com/nxyf/shardingsphere/ShardingSphereApplicationTests.java
@@ -1,10 +1,13 @@
 package com.nxyf.shardingsphere;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.nxyf.shardingsphere.entity.Course;
 import com.nxyf.shardingsphere.mapper.CourseMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
 
 @SpringBootTest
 class ShardingSphereApplicationTests {
@@ -13,13 +16,26 @@ class ShardingSphereApplicationTests {
     private CourseMapper courseMapper;
 
     @Test
-    void contextLoads() {
+    void addCourse() {
         for (int i = 0; i < 10; i++) {
             Course course = new Course();
             course.setCname("java"+i);
             course.setUserId(1010L);
             course.setCstatus("noral");
             courseMapper.insert(course);
+        }
+
+    }
+
+    @Test
+    void queryCourse() {
+        QueryWrapper<Course> queryWrapper = new QueryWrapper<>();
+        /*queryWrapper.eq("cid", 594834418481233920L);
+        Course course = courseMapper.selectOne(queryWrapper);*/
+//        System.out.printf("=========="+course+"------------");
+        List<Course> courses = courseMapper.selectList(queryWrapper);
+        for (Course cours : courses) {
+            System.out.printf("=========="+cours+"------------");
         }
 
     }
